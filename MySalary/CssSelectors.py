ef5dcbdfb71ea9bbb0f93ef3a10eff72e50e6a8b
IamIdan <ferb1124@gmail.com>
2019-10-19 17:14:13 +0300

    Updated css selectors

--- MySalary/CssSelectors.py
+++ MySalary/CssSelectors.py
@@ -5,4 +5,15 @@ NEXT_MONTH = '#Header1_MonthAndYearBrowser1_imgbtnNextMonth'
 ADD_REPORT_TABLE = 'table td[valign=top] > table:not(.main_grid)'  # for easy selection of children inside it
 
 # select best practice: https://stackoverflow.com/a/28613320/7320123
+DROPDOWN_CUSTOMER = 'select[name="ddlCustomers"]'
+DROPDOWN_PROJECT = 'select[name="ddlProjects"]'
+DROPDOWN_TASK = 'select[name="ddlTasks"]'
 
+TEXT_ELABORATION = 'textarea[name=txtElaboration]'  # פירוט
+OPTIONAL_DAYS = 'td.calDay'  # returns all the possible days to select in the moths, without the selected day.
+SELECTED_DAY = '#tdSelectedDay'
+
+START_HOURS_INPUT = '#txtFromHours'
+START_MINUTES_INPUT = '#txtFromMinutes'
+END_HOURS_INPUT = '#txtToHours'
+END_MINUTES_INPUT = '#txtToMinutes'
