9b7b3f7fed713768080d8a2ba72e50896496ce52
IamIdan <ferb1124@gmail.com>
2019-10-19 13:15:46 +0300

    Added css-selctors files

---
+++ MySalary/CssSelectors.py
@@ -0,0 +1,8 @@
+CURRENT_YEAR = '#Header1_MonthAndYearBrowser1_spnYear'  # last 2 numbers. (for example 19)
+CURRENT_MONTH = '#Header1_MonthAndYearBrowser1_spnMonth'  # as hebrew text (נובמבר)
+PER_MONTH = '#Header1_MonthAndYearBrowser1_imgbtnPrevMonth'
+NEXT_MONTH = '#Header1_MonthAndYearBrowser1_imgbtnNextMonth'
+ADD_REPORT_TABLE = 'table td[valign=top] > table:not(.main_grid)'  # for easy selection of children inside it
+
+# select best practice: https://stackoverflow.com/a/28613320/7320123
+
