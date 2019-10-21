d4824259315a33b8e85834a8eb7d9838ab0956c1
IamIdan <ferb1124@gmail.com>
2019-10-21 12:26:55 +0300

    WIP, added report shift function, and started working on navigating to the right month

--- MySalary/SeleniumRequestManger.py
+++ MySalary/SeleniumRequestManger.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from . import CssSelectors as cs
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 
@@ -32,6 +34,48 @@ class SeleniumManager:
         except TimeoutException:
             self.driver.close()
             raise TimeoutException("Timeout. Check your credentials or your internet connection")
+
+    def report_shift(self, start_date: datetime, end_date: datetime):
+        """
+        Report a shift into a logged in Deshe session.
+        :param start_date: Datetime object
+        :param end_date: Datetime object
+        :return:
+        """
+        # Due to the Deshe system, cannot report shifts on a couple of days.
+        if start_date.month != end_date.month or start_date.day != end_date.day:
+            raise ValueError("Cannot report shifts on multiple days")
+
+    def navigate_to_month(self, month, year):
+        """
+        Navigating the Deshe page to the requested month.
+        :param month: the desired month, as a number.
+        :param year: number representing the last 2 digits of the dedired year (2019 will be represented as 19)
+        :return:
+        """
+        pass
+
+    def get_current_month(self):
+        """
+        Get the current displayed month, in numbers.
+        :return: number representing the current month
+        """
+        current_month = self.driver.find_element_by_css_selector(cs.CURRENT_MONTH)
+        months_to_numbers = {
+            'ינואר': 1,
+            'פבואר': 2,
+            'מרץ': 3,
+            'אפריל': 4,
+            'מאי': 5,
+            'יוני': 6,
+            'יולי': 7,
+            'אוגוסט': 8,
+            'ספטמבר': 9,
+            'אוקטובר': 10,
+            'נובמבר': 11,
+            'דצמבר': 12,
+        }
+        return months_to_numbers[current_month]
 
 
 SeleniumManager('idanru', '123456', headless=False)
